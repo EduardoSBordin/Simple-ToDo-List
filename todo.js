898a719443307c9641e91ea1482ebee364f824de
Eduardo Bordin <dudubordin26@gmail.com>
2022-04-22 01:26:06 -0300

    More some things

--- todo.js
+++ todo.js
@@ -3,9 +3,22 @@ const textSquare = document.getElementById('textSquare');
 const getDiv =  document.getElementById('newDiv');
 const btnDeleteAll = document.getElementById('btnDeleteAll').onclick = () => {document.location.reload()};
 
+let btnRemove = document.getElementById("btnRemove");
+
+
+let pTest = document.getElementById('pTest');
+let c = 0;
+
 btnAdd.onclick = (e) => {
 
-    e.preventDefault();
+    e.preventDefault(); 
+    
+    c++;
+    pTest.innerHTML = `5/${c}`;
+
+    if(c >= 5){
+        btnAdd.style.display = "none";
+    }
 
     let newDiv = document.createElement('input');
     newDiv.id = 'newDiv';
@@ -20,11 +33,16 @@ btnAdd.onclick = (e) => {
     btnRemove.onclick = () =>{
         newDiv.remove();
         btnRemove.remove();
+        btnAdd.style.display = "inline-block";
+        c -= 1;
+        pTest.innerHTML = `5/${c}`;
     }
     //###############################
 
     textSquare.appendChild(newDiv);
-    textSquare.appendChild(btnRemove);  
+    textSquare.appendChild(btnRemove); 
+
+
 }
 
 
