017e399621e3170ed26c60d83f9f6e13fe110737
Eduardo Bordin <dudubordin26@gmail.com>
2022-04-22 03:22:37 -0300

    Fixed some things.

--- todo.js
+++ todo.js
@@ -1,9 +1,18 @@
 const btnAdd = document.getElementById('btnAdd');
+const btnTheme = document.getElementById('btnTheme');
+const bodyT = document.getElementById('bodyT');
 const textSquare = document.getElementById('textSquare');
 const getDiv =  document.getElementById('newDiv');
+const container = document.getElementById('container');
 
 
 //let btnRemove = document.getElementById("btnRemove");
+
+btnTheme.onclick = () => {
+
+    bodyT.classList.toggle('active');
+    container.classList.toggle('active');
+}
 
 let pTest = document.getElementById('pTest');
 let c = 0;
